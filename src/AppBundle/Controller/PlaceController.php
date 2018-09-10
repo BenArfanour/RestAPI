14e1e55b5109949b0f7acf940cc8d50c5b958d75
hamza <benarfanour9@gmail.com>
2018-09-10 15:53:53 +0100

    rest API Symfony

---
+++ src/AppBundle/Controller/PlaceController.php
@@ -0,0 +1,168 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations\Get; 
+use FOS\RestBundle\Controller\Annotations\Put;// N'oublons pas d'inclure Get
+use FOS\RestBundle\View\View; // Utilisation de la vue de FOSRestBundle
+use FOS\RestBundle\Controller\Annotations as Rest; // alias pour toutes les annotations
+use FOS\RestBundle\View\ViewHandler;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Form\PlaceType;
+use AppBundle\Entity\Place;
+
+class PlaceController extends Controller
+{
+
+    /**
+     * @Rest\View(serializerGroups={"place"})
+     * @Rest\Get("/places")
+     */
+
+    public function getPlacesAction(Request $request)
+    {
+        $places = $this->get('doctrine.orm.entity_manager')->getRepository('AppBundle:Place')->findAll();
+        
+
+        return $places;
+    }
+
+
+
+
+        /**
+         * @Rest\View(serializerGroups={"place"})
+         * @Rest\Get("/places/{id}")
+         */
+
+    public function getPlaceAction($id)
+    {
+
+        $place = $this->get('doctrine.orm.entity_manager')->getRepository('AppBundle:Place')->find($id);
+        /* @var $place Place */
+
+                if (empty($place)) {
+            return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
+        }
+        
+
+
+
+        return $place;
+    }
+
+
+   
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED, serializerGroups={"place"})
+     * @Rest\Post("/places")
+     */
+
+    public function postPlacesAction(Request $request)
+    {
+        $place = new Place();
+        $form = $this->createForm(PlaceType::class, $place);
+
+        $form->submit($request->request->all()); // Validation des données
+
+        if ($form->isValid()) {
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->persist($place);
+            $em->flush();
+
+            return $place;
+        } else {
+            return $form;
+        }
+    }
+
+
+
+
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_NO_CONTENT, serializerGroups={"place"})
+     * @Rest\Delete("/places/{id}")
+     */
+    public function removePlaceAction(Request $request)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $place = $em->getRepository('AppBundle:Place')
+                    ->find($request->get('id'));
+        /* @var $place Place */
+
+
+        if (!$place) {
+            return;
+        }
+
+
+         foreach ($place->getPrices() as $price) {
+            $em->remove($price);
+        }
+
+        $em->remove($place);
+        $em->flush();
+       }
+    }
+
+
+
+    /**
+     * @Rest\View(serializerGroups={"place"})
+     * @Rest\Put("/places/{id}")
+     */
+
+    function updatePlaceAction(Request $request)
+    {
+        return $this->updatePlace($request, true);
+    }
+
+    /**
+     * @Rest\View(serializerGroups={"place"})
+     * @Rest\Patch("/places/{id}")
+     */
+   function patchPlaceAction(Request $request)
+    {
+        return $this->updatePlace($request, false);
+    }
+
+
+
+   function updatePlace(Request $request,$clearMissing)
+
+    {
+        $place = $this->get('doctrine.orm.entity_manager')->getRepository('AppBundle:Place')->find($request->get('id'));
+
+         // L'identifiant en tant que paramètre n'est plus nécessaire
+        /* @var $place Place */
+
+
+        if (empty($place)) {
+            return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $form = $this->createForm(PlaceType::class, $place);
+
+        $form->submit($request->request->all(),$clearMissing);
+
+        if ($form->isValid())  
+         {
+            $em = $this->get('doctrine.orm.entity_manager');
+            // l'entité vient de la base, donc le merge n'est pas nécessaire.
+            // il est utilisé juste par soucis de clarté
+            $em->merge($place);
+            $em->flush();
+            return $place;
+         } 
+         else {
+            return $form;
+        }
+    }
+      
+
+
---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,212 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+use FOS\RestBundle\Controller\Annotations\Get; // N'oublons pas d'inclure Get
+use FOS\RestBundle\View\View; // Utilisation de la vue de FOSRestBundle
+use FOS\RestBundle\Controller\Annotations as Rest; // alias pour toutes les annotations
+use FOS\RestBundle\View\ViewHandler;
+use AppBundle\Form\UserType;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+
+
+
+
+     /**
+     * @Rest\View()
+     * @Rest\Get("/users")
+     */
+
+    public function getUsersAction()
+    {
+        $users = $this->get('doctrine.orm.entity_manager')->getRepository('AppBundle:User')->findAll();
+
+
+        
+        
+        return $users;
+    }
+
+
+   /**
+     * @Rest\View()
+     * @Rest\Get("/users/{id}")
+     */
+
+    public function getUserAction($id)
+    {
+        $user = $this->get('doctrine.orm.entity_manager')->getRepository('AppBundle:User')->find($id);
+
+
+        if (empty($user)) {
+            return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+
+  
+
+        return $user ;
+    }
+
+
+
+      /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED)
+     * @Rest\Post("/users")
+     */
+    public function postUsersAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->submit($request->request->all());
+
+        if ($form->isValid()) {
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->persist($user);
+            $em->flush();
+            return $user;
+        } else {
+            return $form;
+        }
+    }
+
+
+
+     /**
+     * @Rest\View(statusCode=Response::HTTP_NO_CONTENT)
+     * @Rest\Delete("/users/{id}")
+     */
+    public function removeUserAction(Request $request)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $user = $em->getRepository('AppBundle:User')
+                    ->find($request->get('id'));
+        /* @var $user User */
+
+        if ($user) {
+            $em->remove($user);
+            $em->flush();
+        }
+    }
+
+
+     /**
+     * @Rest\View()
+     * @Rest\Put("/users/{id}")
+     */
+    public function updateUserAction(Request $request)
+    {
+        $user = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AppBundle:User')
+                ->find($request->get('id')); // L'identifiant en tant que paramètre n'est plus nécessaire
+        /* @var $user User */
+
+        if (empty($user)) {
+            return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->submit($request->request->all());
+
+        if ($form->isValid()) {
+            $em = $this->get('doctrine.orm.entity_manager');
+            // l'entité vient de la base, donc le merge n'est pas nécessaire.
+            // il est utilisé juste par soucis de clarté
+            $em->merge($user);
+            $em->flush();
+            return $user;
+        } else {
+            return $form;
+        }
+    }        
+
+
+
+
+      /**
+     * @Rest\View()
+     * @Rest\Patch("/users/{id}")
+     */
+    public function patchUserAction(Request $request)
+    {
+        return $this->updateUser($request, false);
+    }
+
+    private function updateUser(Request $request, $clearMissing)
+    {
+        $user = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AppBundle:User')
+                ->find($request->get('id')); // L'identifiant en tant que paramètre n'est plus nécessaire
+        /* @var $user User */
+
+        if (empty($user)) {
+            return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->submit($request->request->all(), $clearMissing);
+
+        if ($form->isValid()) {
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->persist($user);
+            $em->flush();
+            return $user;
+        } else {
+            return $form;
+        }
+    }
+
+
+
+
+    /**
+     * @Rest\View(serializerGroups={"place"})
+     * @Rest\Get("/users/{id}/suggestions")
+     */
+    public function getUserSuggestionsAction(Request $request)
+    {
+        $user = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AppBundle:User')
+                ->find($request->get('id'));
+        /* @var $user User */
+
+        if (empty($user)) {
+            return $this->userNotFound();
+        }
+
+        $suggestions = [];
+
+        $places = $this->get('doctrine.orm.entity_manager')
+                ->getRepository('AppBundle:Place')
+                ->findAll();
+
+        foreach ($places as $place) {
+            if ($user->preferencesMatch($place->getThemes())) {
+                $suggestions[] = $place;
+            }
+        }
+
+        return $suggestions;
+    }
+
+
+
+     private function userNotFound()
+    {
+        return \FOS\RestBundle\View\View::create(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+    }
+
+}
